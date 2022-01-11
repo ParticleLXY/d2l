95d39621fecc351a3fa7b2a66657e0390f44dfa2
agave <skyquantum@outlook.com>
2022-01-11 21:17:54 +0800

    first commit

---
+++ d2l.py
@@ -0,0 +1,391 @@
+from re import X
+import matplotlib
+from matplotlib import pyplot as plt, use
+from IPython import display
+import time
+import numpy as np
+import tensorflow as tf
+import hashlib
+import os
+import tarfile
+import zipfile
+import requests
+
+def use_svg_display():
+    display.set_matplotlib_formats('svg')
+
+def set_figsize(figsize=(3.5,2.5)):
+    use_svg_display()
+    plt.rcParams['figure.figsize'] = figsize
+
+def set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend):
+    """设置matplotlib的轴"""
+    axes.set_xlabel(xlabel)
+    axes.set_ylabel(ylabel)
+    axes.set_xscale(xscale)
+    axes.set_yscale(yscale)
+    axes.set_xlim(xlim)
+    axes.set_ylim(ylim)
+    if legend:
+        axes.legend(legend)
+    axes.grid()
+
+def plot(X, Y=None, xlabel=None, ylabel=None, legend=None, xlim=None,
+         ylim=None, xscale='linear', yscale='linear',
+         fmts=('-', 'm--', 'g-.', 'r:'), figsize=(3.5, 2.5), axes=None):
+    """绘制数据点"""
+    if legend is None:
+        legend = []
+
+    set_figsize(figsize)
+    axes = axes if axes else plt.gca()
+
+    # 如果X有一个轴，输出True
+    def has_one_axis(X):
+        return (hasattr(X, "ndim") and X.ndim == 1 or isinstance(X, list)
+                and not hasattr(X[0], "__len__"))
+
+    if has_one_axis(X):
+        X = [X]
+    if Y is None:
+        X, Y = [[]] * len(X), X
+    elif has_one_axis(Y):
+        Y = [Y]
+    if len(X) != len(Y):
+        X = X * len(Y)
+    axes.cla()
+    for x, y, fmt in zip(X, Y, fmts):
+        if len(x):
+            axes.plot(x, y, fmt)
+        else:
+            axes.plot(y, fmt)
+    set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
+
+class Timer:
+    '''记录多次运行时间'''
+    def __init__(self) -> None:
+        self.times = []
+        self.start()
+
+    def start(self):
+        '''启动计时器'''
+        self.tik = time.time()
+
+    def stop(self):
+        '''停止计时器并记录在列表中'''
+        self.times.append(time.time()-self.tik)
+        return self.times[-1]
+
+    def avg(self):
+        '''返回平均时间'''
+        return sum(self.times)/len(self.times)
+
+    def sum(self):
+        '''返回总时间'''
+        return sum(self.times)
+
+    def cumsum(self):
+        '''返回累计时间'''
+        return np.array(self.times).cumsum().tolist()
+
+def synthetic_data(w,b,num_examples):
+    '''生成y=Xw+b+噪声'''
+    X = tf.zeros((num_examples,w.shape[0]))
+    X += tf.random.normal(shape=X.shape)
+    y = tf.matmul(X,tf.reshape(w,(-1,1)))+b
+    y += tf.random.normal(shape=y.shape,stddev=0.01)
+    y = tf.reshape(y,(-1,1))
+    return X,y
+
+def linreg(X,w,b):
+    '''线性模型回归'''
+    return tf.matmul(X,w)+b
+
+def squared_loss(y_hat, y):
+    """均方损失"""
+    return (y_hat - tf.reshape(y, y_hat.shape)) ** 2 / 2
+
+def sgd(params,grads,lr,batch_size):
+    '''mini batch sgd'''
+    for param,grad in zip(params,grads):
+        param.assign_sub(lr*grad/batch_size)
+
+def get_fashion_mnist_labels(labels):
+    """返回Fashion-MNIST数据集的文本标签"""
+    text_labels = ['t-shirt', 'trouser', 'pullover', 'dress', 'coat',
+                   'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
+    return [text_labels[int(i)] for i in labels]
+
+def show_images(imgs, num_rows, num_cols, titles=None, scale=1.5):
+    """绘制图像列表"""
+    figsize = (num_cols * scale, num_rows * scale)
+    _, axes = plt.subplots(num_rows, num_cols, figsize=figsize)
+    axes = axes.flatten()
+    for i, (ax, img) in enumerate(zip(axes, imgs)):
+        ax.imshow(img.numpy())
+        ax.axes.get_xaxis().set_visible(False)
+        ax.axes.get_yaxis().set_visible(False)
+        if titles:
+            ax.set_title(titles[i])
+    return axes
+
+def load_array(data_arrays, batch_size, is_train=True):
+    """构造一个TensorFlow数据迭代器"""
+    dataset = tf.data.Dataset.from_tensor_slices(data_arrays)
+    if is_train:
+        dataset = dataset.shuffle(buffer_size=1000)
+    dataset = dataset.batch(batch_size)
+    return dataset
+
+def load_data_fashion_mnist(batch_size,resize=None):
+    '''下载Fashion-MNIST数据集，然后加载到内存中'''
+    mnist_train,mnist_test = tf.keras.datasets.fashion_mnist.load_data()
+    # 归一化，增加一个批处理维度，标签转换为int32
+    process = lambda X,y:(tf.expand_dims(X,axis=3)/255,tf.cast(y,dtype='int32'))
+    resize_fn = lambda X,y:(tf.image.resize_with_pad(X,resize,resize)if resize else X,y)
+    return (
+        tf.data.Dataset.from_tensor_slices(process(*mnist_train)).batch(batch_size).shuffle\
+        (len(mnist_train[0])).map(resize_fn),tf.data.Dataset.from_tensor_slices(process(*mnist_test))\
+            .batch(batch_size).map(resize_fn)
+    )
+
+def accuracy(y_hat, y):
+    """计算预测正确的数量"""
+    if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
+        y_hat = tf.argmax(y_hat, axis=1)
+    cmp = tf.cast(y_hat, y.dtype) == y
+    return float(tf.reduce_sum(tf.cast(cmp, y.dtype)))
+
+def evaluate_accuracy(net, data_iter):
+    """计算在指定数据集上模型的精度"""
+    metric = Accumulator(2)  # 正确预测数、预测总数
+    for X, y in data_iter:
+        metric.add(accuracy(net(X), y), tf.size(y))
+    return metric[0] / metric[1]
+    
+class Accumulator:
+    def __init__(self,n) -> None:
+        self.data = [0.0]*n
+
+    def add(self,*args):
+        self.data = [a+float(b) for a,b in zip(self.data,args)]
+
+    def reset(self):
+        self.data = [0.0]*len(self.data)
+
+    def __getitem__(self,idx):
+        return self.data[idx]
+
+def train_epoch_ch3(net,train_iter,loss,updater):
+    metric = Accumulator(3)
+    for X,y in train_iter:
+        with tf.GradientTape() as tape:
+            y_hat = net(X)
+            if isinstance(loss,tf.keras.losses.Loss):
+                l = loss(y,y_hat)
+            else:
+                l = loss(y_hat,y)
+        if isinstance(updater,tf.keras.optimizers.Optimizer):
+            params = net.trainable_variables
+            grads = tape.gradient(l,params)
+            updater.apply_gradients(zip(grads,params))
+        else:
+            updater(X.shape[0],tape.gradient(l,updater.params))
+        l_sum = l*float(tf.size(y)) if isinstance(\
+            loss,tf.keras.losses.Loss) else tf.reduce_sum(l)
+        metric.add(l_sum, accuracy(y_hat, y), tf.size(y))
+    return metric[0]/metric[2],metric[1]/metric[2]
+
+class Animator:
+    '''在动画中绘制数据'''
+    def __init__(self,xlabel=None,ylabel=None,legend=None,xlim=None,ylim=None,xscale='linear',\
+        yscale='linear',fmts=('-','m--','g-','r:'),nrows=1,ncols=1,figsize=(3.5,2.5)) -> None:
+        if legend is None:
+            legend = []
+        use_svg_display()
+        self.fig,self.axes = plt.subplots(nrows,ncols,figsize=figsize)
+        if nrows*ncols == 1:
+            self.axes = [self.axes, ]
+        self.config_axes = lambda: set_axes(
+            self.axes[0],xlabel,ylabel,xlim,ylim,xscale,yscale,legend
+        )
+        self.X,self.Y,self.fmts = None,None,fmts
+    
+    def add(self,x,y):
+        if not hasattr(y,'__len__'):
+            y = [y]
+        n = len(y)
+        if not hasattr(x,'__len__'):
+            x = [x]*n
+        if not self.X:
+            self.X = [[]for _ in range(n)]
+        if not self.Y:
+            self.Y = [[]for _ in range(n)]
+        for i,(a,b) in enumerate(zip(x,y)):
+            if a is not None and b is not None:
+                self.X[i].append(a)
+                self.Y[i].append(b)
+        self.axes[0].cla()
+        for x,y,fmt in zip(self.X,self.Y,self.fmts):
+            self.axes[0].plot(x,y,fmt)
+        self.config_axes()
+        display.display(self.fig)
+        display.clear_output(wait=True)
+
+def train_ch3(net,train_iter,test_iter,loss,num_epochs,updater):
+    animator = Animator(xlabel='epoch',xlim=[1,num_epochs],ylim=[0.3,0.9],\
+        legend=['train loss','train acc','test acc'])
+    for epoch in range(num_epochs):
+        train_matrics = train_epoch_ch3(net,train_iter,loss,updater)
+        test_acc = evaluate_accuracy(net,test_iter)
+        animator.add(epoch+1,train_matrics+(test_acc,))
+    train_loss,train_acc = train_matrics
+    assert train_loss < 0.5, train_loss
+    assert train_acc <= 1 and train_acc > 0.7, train_acc
+    assert test_acc <= 1 and test_acc > 0.7, test_acc
+
+class Updater():
+    """用小批量随机梯度下降法更新参数"""
+    def __init__(self, params, lr):
+        self.params = params
+        self.lr = lr
+
+    def __call__(self, batch_size, grads):
+        sgd(self.params, grads, self.lr, batch_size)
+
+def predict_ch3(net, test_iter, n=6):
+    """预测标签（定义见第3章）"""
+    for X, y in test_iter:
+        break
+    trues = get_fashion_mnist_labels(y)
+    preds = get_fashion_mnist_labels(tf.argmax(net(X), axis=1))
+    titles = [true +'\n' + pred for true, pred in zip(trues, preds)]
+    show_images(
+        tf.reshape(X[0:n], (n, 28, 28)), 1, n, titles=titles[0:n])
+
+def evaluate_loss(net, data_iter, loss):
+    """评估给定数据集上模型的损失"""
+    metric = Accumulator(2)  # 损失的总和,样本数量
+    for X, y in data_iter:
+        l = loss(net(X), y)
+        metric.add(tf.reduce_sum(l), tf.size(l))
+    return metric[0] / metric[1]
+
+DATA_HUB = dict()
+DATA_URL = 'http://d2l-data.s3-accelerate.amazonaws.com/'
+
+def download(name,cache_dir=os.path.join('..','data')):
+    assert name in DATA_HUB,f"{name}不存在于{DATA_HUB}"
+    url,sha1_hash = DATA_HUB[name]
+    os.makedirs(cache_dir,exist_ok=True)
+    fname = os.path.join(cache_dir,url.split('/')[-1])
+    if os.path.exists(fname):
+        sha1 = hashlib.sha1()
+        with open(fname,'rb') as f:
+            while True:
+                data = f.read(1048576)
+                if not data:
+                    break
+                sha1.update(data)
+        if sha1.hexdigest() ==sha1_hash:
+            return fname
+    print(f'正在从{url}下载{fname}...')
+    r = requests.get(url,stream=True,verify=True)
+    with open(fname,'wb') as f:
+        f.write(r.content)
+    return fname
+
+def download_extract(name,folder=None):
+    fname = download(name)
+    base_dir = os.path.dirname(fname)
+    data_dir,ext = os.path.splitext(fname)
+    if ext == 'zip':
+        fp = zipfile.ZipFile(fname,'r')
+    elif ext in ('.tar','.gz'):
+        fp = tarfile.open(fname, 'r')
+    else:
+        assert False,'只有zip/tar文件可以被压缩'
+    fp.extractall(base_dir)
+    return os.path.join(base_dir, folder) if folder else data_dir
+
+def download_all():
+    """下载DATA_HUB中的所有文件"""
+    for name in DATA_HUB:
+        download(name)
+
+def try_gpu(i=0):
+    """如果存在，则返回gpu(i)，否则返回cpu()"""
+    if len(tf.config.experimental.list_physical_devices('GPU')) >= i + 1:
+        return tf.device(f'/GPU:{i}')
+    return tf.device('/CPU:0')
+
+def try_all_gpus():
+    """返回所有可用的GPU，如果没有GPU，则返回[cpu(),]"""
+    num_gpus = len(tf.config.experimental.list_physical_devices('GPU'))
+    devices = [tf.device(f'/GPU:{i}') for i in range(num_gpus)]
+    return devices if devices else [tf.device('/CPU:0')]
+
+class TrainCallback(tf.keras.callbacks.Callback):
+    """一个以可视化的训练进展的回调"""
+    def __init__(self, net, train_iter, test_iter, num_epochs, device_name):
+        self.timer = Timer()
+        self.animator = Animator(
+            xlabel='epoch', xlim=[1, num_epochs], legend=[
+                'train loss', 'train acc', 'test acc'])
+        self.net = net
+        self.train_iter = train_iter
+        self.test_iter = test_iter
+        self.num_epochs = num_epochs
+        self.device_name = device_name
+
+    def on_epoch_begin(self, epoch, logs=None):
+        self.timer.start()
+
+    def on_epoch_end(self, epoch, logs):
+        self.timer.stop()
+        test_acc = self.net.evaluate(
+            self.test_iter, verbose=0, return_dict=True)['accuracy']
+        metrics = (logs['loss'], logs['accuracy'], test_acc)
+        self.animator.add(epoch + 1, metrics)
+        if epoch == self.num_epochs - 1:
+            batch_size = next(iter(self.train_iter))[0].shape[0]
+            num_examples = batch_size * tf.data.experimental.cardinality(
+                self.train_iter).numpy()
+            print(f'loss {metrics[0]:.3f}, train acc {metrics[1]:.3f}, '
+                  f'test acc {metrics[2]:.3f}')
+            print(f'{num_examples / self.timer.avg():.1f} examples/sec on '
+                  f'{str(self.device_name)}')
+
+
+def train_ch6(net_fn, train_iter, test_iter, num_epochs, lr, device):
+    """用GPU训练模型(在第六章定义)"""
+    device_name = device._device_name
+    strategy = tf.distribute.OneDeviceStrategy(device_name)
+    with strategy.scope():
+        optimizer = tf.keras.optimizers.SGD(learning_rate=lr)
+        loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+        net = net_fn()
+        net.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
+    callback = TrainCallback(net, train_iter, test_iter, num_epochs,
+                             device_name)
+    net.fit(train_iter, epochs=num_epochs, verbose=0, callbacks=[callback])
+    return net
+
+def show_heatmaps(matrices, xlabel, ylabel, titles=None, figsize=(2.5, 2.5),
+                  cmap='Reds'):
+    """显示矩阵热图"""
+    use_svg_display()
+    num_rows, num_cols = matrices.shape[0], matrices.shape[1]
+    fig, axes = plt.subplots(num_rows, num_cols, figsize=figsize,
+                                 sharex=True, sharey=True, squeeze=False)
+    for i, (row_axes, row_matrices) in enumerate(zip(axes, matrices)):
+        for j, (ax, matrix) in enumerate(zip(row_axes, row_matrices)):
+            pcm = ax.imshow(matrix.numpy(), cmap=cmap)
+            if i == num_rows - 1:
+                ax.set_xlabel(xlabel)
+            if j == 0:
+                ax.set_ylabel(ylabel)
+            if titles:
+                ax.set_title(titles[j])
+    fig.colorbar(pcm, ax=axes, shrink=0.6);
+
