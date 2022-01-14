073b59c0f05bee51f96b8be335bb67aa45cb603a
agave <skyquantum@outlook.com>
2022-01-14 13:32:26 +0800

    1.14

--- d2l.py
+++ d2l.py
@@ -391,15 +391,73 @@ def show_heatmaps(matrices, xlabel, ylabel, titles=None, figsize=(2.5, 2.5),
                 ax.set_title(titles[j])
     fig.colorbar(pcm, ax=axes, shrink=0.6);
 
+'''
+chapter8 循环神经网络
+'''
+
 def read_time_machine():
+    '''读取数据集'''
     with open(download('time_machine'),'r') as f:
         lines = f.readlines()
     return [re.sub('[^A-Za-z]+', ' ', line).strip().lower() for line in lines]
 
 def tokenize(lines,token='word'):
+    '''将文本行拆分为单词或字符词元'''
     if token == 'word':
         return [line.split() for line in lines]
     elif token == 'char':
         return [list(line) for line in lines]
     else:
         print('错误：未知词元类型：'+token)
+
+def count_corpus(tokens):
+    if len(tokens) == 0 or isinstance(tokens[0],list):
+        tokens = [token for line in tokens for token in line]
+    return collections.Counter(tokens)
+
+class Vocab:
+    '''词表'''
+    def __init__(self,tokens=None,min_freq=0,reserved_tokens=None):
+        if tokens is None:
+            tokens = []
+        if reserved_tokens is None:
+            reserved_tokens = []
+        counter = count_corpus(tokens)
+        self._token_freqs = sorted(counter.items(),key=lambda x:x[1],reverse=True)
+        self.idx_to_token = ['<unk>']+reserved_tokens
+        self.token_to_idx = {token:idx for idx,token in enumerate(self.idx_to_token)}
+        self.idx_to_token,self.token_to_idx = [],dict()
+        for token,freq in self._token_freqs:
+            if freq<min_freq:
+                break
+            if token not in self.token_to_idx:
+                self.idx_to_token.append(token)
+                self.token_to_idx[token] = len(self.idx_to_token)-1
+
+    def __len__(self):
+        return len(self.idx_to_token)
+    
+    def __getitem__(self,tokens):
+        if not isinstance(tokens, (list, tuple)):
+            return self.token_to_idx.get(tokens, self.unk)
+        return [self.__getitem__(token) for token in tokens]
+
+    @property
+    def unk(self):  # 未知词元的索引为0
+        return 0
+
+    @property
+    def token_freqs(self):
+        return self._token_freqs
+
+def load_corpus_time_machine(max_tokens=-1):
+    """返回时光机器数据集的词元索引列表和词表"""
+    lines = read_time_machine()
+    tokens = tokenize(lines, 'char')
+    vocab = Vocab(tokens)
+    # 因为时光机器数据集中的每个文本行不一定是一个句子或一个段落，
+    # 所以将所有文本行展平到一个列表中
+    corpus = [vocab[token] for line in tokens for token in line]
+    if max_tokens > 0:
+        corpus = corpus[:max_tokens]
+    return corpus, vocab
